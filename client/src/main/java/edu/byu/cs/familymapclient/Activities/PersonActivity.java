f07140b67add8802fe06de29f856a38f5c7792ed
Jake Dustin <jakedustin097@gmail.com>
2021-04-13 21:48:08 -0600

    added text fields to person activity layout file

--- client/src/main/java/edu/byu/cs/familymapclient/Activities/PersonActivity.java
+++ client/src/main/java/edu/byu/cs/familymapclient/Activities/PersonActivity.java
@@ -1,6 +1,15 @@
 package edu.byu.cs.familymapclient.Activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 }
